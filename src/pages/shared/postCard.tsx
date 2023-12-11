26a31974696a73888e335bf4b45e7c3879799387
imran <imran.h.ovi.95@gmail.com>
2023-12-11 22:13:46 +0600

    trying to reach errors on deployment - debugging

--- src/pages/shared/postCard.tsx
+++ src/pages/shared/postCard.tsx
@@ -20,14 +20,14 @@ const PostCard = ({ post }: PostCardProps) => {
   const { user } = useUserContext();
   const { isPending: isPostLoading } = useGetRecentPosts();
 
-  if (!post.creator) return;
+  if (!post || !user) return;
 
   return (
     <div className="overflow-hidden border shadow-lg dark:border-gray-500 rounded-xl ">
       <div className="">
         <div className="flex justify-between w-full gap-2 p-2">
           <Link
-            href={`/profile/${post.creator.$id}`}
+            href={`/profile/${post?.creator.$id}`}
             className="flex items-center gap-2"
           >
             <img
@@ -37,7 +37,7 @@ const PostCard = ({ post }: PostCardProps) => {
               loading="lazy"
             />
             <div className="flex flex-col">
-              <p className="text-md">{post.creator.name}</p>
+              <p className="text-md">{post?.creator.name}</p>
               <p className="flex items-center text-sm text-gray-400">
                 <span>{formatDate(post.$createdAt)}</span>
                 <Dot />
@@ -46,17 +46,17 @@ const PostCard = ({ post }: PostCardProps) => {
             </div>
           </Link>
           <Link
-            href={`/routes/editPost/${post.$id}`}
-            className={`${user.id !== post.creator.$id && "hidden"}`}
+            href={`/routes/editPost/${post?.$id}`}
+            className={`${user?.id !== post?.creator.$id && "hidden"}`}
           >
             <PenSquare size={20} />{" "}
           </Link>
         </div>
         <div className="p-2">
-          <Link href={`/shared/postDetails/${post.$id}`}>
-            <p className="">{post.caption}</p>
+          <Link href={`/shared/postDetails/${post?.$id}`}>
+            <p className="">{post?.caption}</p>
             <ul className="flex items-center gap-2 text-sm text-gray-500">
-              {post.tags.map((tag: string, index: number) => (
+              {post?.tags.map((tag: string, index: number) => (
                 <li key={index}>#{tag}</li>
               ))}
             </ul>
@@ -66,7 +66,7 @@ const PostCard = ({ post }: PostCardProps) => {
           <Dialog>
             <DialogTrigger className="w-full">
               <img
-                src={post.imageUrl}
+                src={post?.imageUrl}
                 alt=""
                 className="object-cover w-full h-60"
                 loading="lazy"
@@ -74,7 +74,7 @@ const PostCard = ({ post }: PostCardProps) => {
             </DialogTrigger>
             <DialogContent className="sm:max-w-[500px] p-0">
               <img
-                src={post.imageUrl}
+                src={post?.imageUrl}
                 alt=""
                 className="w-full h-full"
                 loading="lazy"
@@ -83,7 +83,7 @@ const PostCard = ({ post }: PostCardProps) => {
           </Dialog>
         </div>
         <div className="px-5 py-2">
-          <PostStats post={post} userId={user.id} />
+          <PostStats post={post} userId={user?.id} />
         </div>
       </div>
     </div>
